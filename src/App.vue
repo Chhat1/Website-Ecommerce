63f8c40f748d271d26000adddbeec01751c6ce29
Keo Bunthai <Keobunthai0964985054@gmail.com>
2026-05-21 00:31:10 +0700

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script setup>
 import Navbar from './components/layout/Navbar.vue';
 import { useDarkModeStore } from './stores/darkMode';
-
+import Footer from './components/layout/Footer.vue'
 const mode = useDarkModeStore()
 
 </script>
 
 <template>
 
-
   <div :class="mode.darkMode ? 'bg-[#0f172a] text-white' : 'bg-white text-black' ">
       <Navbar/>
       <router-view></router-view>
+      <Footer/>
   </div>
 
 
--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,7 +1,63 @@
 <script setup>
-
+import '@/assets/css/footer.css'
 </script>
-
 <template>
-  
+   <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <!-- colume1 -->
+                <div class="col-12 col-md-6 col-lg-3">
+                    <h2>ELYSIA</h2>
+                    <p>Redefining the intersection of high-fashion craftsmanship and next-generation technology.</p>
+                    <div class="social-icons">
+                        <i class="bi bi-globe"></i>
+                        <i class="bi bi-at"></i>
+                        <i class="bi bi-link-45deg"></i>
+                    </div>
+                </div>
+
+                <!-- colume2 -->
+                <div class="col-12 col-md-6 col-lg-3">
+                    <h1>COMPANY</h1>
+                    <ul>
+                        <li class="list-unstyled"><a href="" class="text-decoration-none ">Sustainability</a></li>
+                        <li class="list-unstyled"><a href="" class="text-decoration-none ">Journal</a></li>
+                        <li class="list-unstyled"><a href="" class="text-decoration-none ">Careers</a></li>
+                        <li class="list-unstyled"><a href="" class="text-decoration-none ">Store Locator</a></li>
+                    </ul>
+                </div>
+
+                <!-- colume3 -->
+                <div class="col-12 col-md-6 col-lg-3">
+                    <h1>SUPPORT</h1>
+                    <ul>
+                        <li class="list-unstyled"><a href="" class="text-decoration-none">Shipping & Returns</a></li>
+                        <li class="list-unstyled"><a href="" class="text-decoration-none">Contact Us</a></li>
+                        <li class="list-unstyled"><a href="" class="text-decoration-none">Privacy Policy</a></li>
+                        <li class="list-unstyled"><a href="" class="text-decoration-none">Terms of Service</a></li>
+                    </ul>
+                </div>
+
+                <!-- colume4 -->
+                <div class="col-12 col-md-6 col-lg-3">
+                    <h1>EXCLUSIVE ACCESS</h1>
+                    <p>Download the Elysia App for a seamless shopping experience.</p>
+
+                    <button class="btn btn-dark store-btn">
+                        <i class="bi bi-phone me-2"></i>
+                        App Store
+                    </button>
+
+                    <button class="btn btn-light border store-btn">
+                        <i class="bi bi-google-play me-2"></i>
+                        Play Store
+                    </button>
+                </div>
+            </div>
+
+            <div class="copyright">
+                © 2024 ELYSIA RETAIL GROUP. ALL RIGHTS RESERVED.
+            </div>
+        </div>
+    </footer>
 </template>
